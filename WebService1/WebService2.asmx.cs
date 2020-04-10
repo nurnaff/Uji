04db00ba5b01576e7e98fad61fff78b23815877e
AndiNurnaf-PC\AndiNurnaf <mynaff26@gmail.com>
2020-04-10 12:13:39 +0700

    create web service c#

---
+++ WebService1/WebService2.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+
+namespace WebService1
+{
+    /// <summary>
+    /// Summary description for WebService2
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService2 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public bool isPalindrome(string s)
+        {
+            char[] ss = s.ToCharArray();
+            Array.Reverse(ss);
+            return(new string(ss)==s)?true:false;
+        }
+    }
+}
